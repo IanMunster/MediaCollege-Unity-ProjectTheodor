418e7fc7655d60091a791d71c1025ddaddcca432
Ian Munster <Munster@live.nl>
2022-07-22 00:17:34 +0200

    Project Theodor the Game
    Project Theodor the Game made with Unity
    made during Media and Game Development study

---
+++ Game/ProjectTheodor/Assets/TestingAssets/Scripts/SimpleMovement.cs
@@ -0,0 +1,18 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class SimpleMovement : MonoBehaviour {
+
+	private float _mySpeed = 10f;
+	private Rigidbody _myRigidbody;
+
+	// Use this for initialization
+	void Start () {
+		_myRigidbody = GetComponent<Rigidbody> ();
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		_myRigidbody.velocity = new Vector3(Input.GetAxis ("Horizontal")*_mySpeed, Input.GetAxis ("Vertical")*_mySpeed,0f);
+	}
+}
--- README.md
+++ README.md
@@ -1,2 +1,8 @@
-# MediaCollege-Unity-ProjectTheodor
- ...
+Project Theodor v1
+==
+
+This is an ambitious game project started by me, Ian Munster
+Currently studying Game-Programming in the Netherlands (Media College, Amsterdam Sloterdijk)
+
+The idea is to create a RolePlayingGame (RPG) that has the crusial Roleplaying aspects.
+like having multiple outcomes of the same problem based on the players decisions. 
